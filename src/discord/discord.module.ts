81c5e616c06278214540f1f4d9185dbc0a7b9ecd
nellupop <holmesmcindoe@gmail.com>
2026-05-01 22:51:14 +0100

    Refactor Discord gateway and providers (module)

--- src/discord/discord.module.ts
+++ src/discord/discord.module.ts
@@ -1,9 +1,14 @@
 import { Global, Module } from '@nestjs/common';
 import { DiscordGateway } from './discord.gateway';
 import { DiscordNotificationService } from './services/discord-notification.service';
+import { DiscordThreadService } from './services/discord-thread.service';
 import { DiscordXpService } from './services/discord-xp.service';
 import { DiscordRoleService } from './services/discord-role.service';
 import { DiscordGuildService } from './services/discord-guild.service';
+import { DiscordMessageService } from './services/discord-message.service';
+import { DiscordInteractionService } from './services/discord-interaction.service';
+import { DiscordReactionService } from './services/discord-reaction.service';
+import { DiscordMemberService } from './services/discord-member.service';
 import { DiscordSetupService } from './handlers/discord-setup.service';
 import { DiscordVerifyService } from './handlers/discord-verify.service';
 import { OnboardingCommand } from './commands/onboarding';
@@ -21,9 +26,14 @@ import { AiModule } from '../ai/ai.module';
   providers: [
     DiscordGateway,
     DiscordNotificationService,
+    DiscordThreadService,
     DiscordXpService,
     DiscordRoleService,
     DiscordGuildService,
+    DiscordMessageService,
+    DiscordInteractionService,
+    DiscordReactionService,
+    DiscordMemberService,
     DiscordSetupService,
     DiscordVerifyService,
     OnboardingCommand,
